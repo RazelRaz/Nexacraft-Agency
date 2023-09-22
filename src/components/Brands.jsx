37d1c804392731690f7878c8e5e8ee64a662696f
RazelRaz <razel16dcm@gmail.com>
2023-09-22 16:00:35 +0600

    dynamic brands added

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,7 +5,6 @@ async function getData(){
         throw new Error("BrandList Calling Failed")
     }
     return res.json();
-
 }
 
 
@@ -21,42 +20,24 @@ const Brands = async () => {
                     Trusted by brands all over the world
                 </h3>
                 <div className="flex flex-wrap -mx-2">
-                    <div className="mb-12 lg:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
-                        <a href="#">
-                            <img
-                                className="mx-auto h-8"
-                                src="atis-assets/logo/brands/slack.png"
-                                alt=""
-                            />
-                        </a>
-                    </div>
-                    <div className="mb-12 lg:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
-                        <a href="#">
-                            <img
-                                className="mx-auto h-8"
-                                src="atis-assets/logo/brands/dropbox.png"
-                                alt=""
-                            />
-                        </a>
-                    </div>
-                    <div className="mb-12 md:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
-                        <a href="#">
-                            <img
-                                className="mx-auto h-8"
-                                src="atis-assets/logo/brands/spotify.png"
-                                alt=""
-                            />
-                        </a>
-                    </div>
-                    <div className="w-full md:w-1/2 lg:w-1/4 px-2">
-                        <a href="#">
-                            <img
-                                className="mx-auto h-8"
-                                src="atis-assets/logo/brands/stripe.png"
-                                alt=""
-                            />
-                        </a>
-                    </div>
+
+                    
+                    {
+                        data.map((singleData, i) => {
+                            return(
+                                <div key={i} className="mb-12 lg:mb-0 w-full md:w-1/2 lg:w-1/4 px-2">
+                                    <a href="#">
+                                        <img
+                                            className="mx-auto h-8"
+                                            src={singleData['image']}
+                                            alt=""
+                                        />
+                                    </a>
+                                </div>
+                            )
+                        })
+                    }
+                    
                 </div>
             </div>
         </section>
